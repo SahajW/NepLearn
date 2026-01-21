9497921b7caa78414519170aece5a014866755f4
Prabin-63 <bashalprabin13@gmail.com>
2026-01-21 16:16:09 +0545

    Aali kati change garya xu

--- backend/database.py
+++ backend/database.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql://postgres:Prabin@localhost:5432/nep_learn_db"
 
-engine = create_engine(DATABASE_URL, echo=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+engine = create_engine(DATABASE_URL, echo=True) #Connects to postgre
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) #Creates datababase sessions
+Base = declarative_base() #base class for models
--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column,Integer,String
 from database import Base #from database.py importing base class
 
 class User(Base):
-    __tablename__= "users"
+    __tablename__= "users" #Table name for database
     id = Column(Integer,primary_key=True,index=True)
     username = Column(String, unique=True, index=True)
     email = Column(String,unique=True, index=True)
